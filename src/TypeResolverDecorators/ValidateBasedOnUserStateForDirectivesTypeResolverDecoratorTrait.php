fe19b130e293d3c84ea060b8573c1efa5bd73e8b
Leo <losoviz@gmail.com>
2020-03-02 15:52:22 +0800

    Access trait through abstract class

--- src/TypeResolverDecorators/ValidateBasedOnUserStateForDirectivesTypeResolverDecoratorTrait.php
+++ src/TypeResolverDecorators/ValidateBasedOnUserStateForDirectivesTypeResolverDecoratorTrait.php
@@ -3,12 +3,9 @@ namespace PoP\UserStateAccessControl\TypeResolverDecorators;
 
 use PoP\AccessControl\Facades\AccessControlManagerFacade;
 use PoP\UserStateAccessControl\Services\AccessControlGroups;
-use PoP\AccessControl\TypeResolverDecorators\ValidateConditionForDirectivesTypeResolverDecoratorTrait;
 
 trait ValidateBasedOnUserStateForDirectivesTypeResolverDecoratorTrait
 {
-    use ValidateConditionForDirectivesTypeResolverDecoratorTrait;
-
     protected function getConfigurationEntries(): array
     {
         $accessControlManager = AccessControlManagerFacade::getInstance();
