b8ad5dfc0b5be7379a69cfa59e9cf7b405bfa825
Leo <losoviz@gmail.com>
2020-03-02 17:21:59 +0800

    Extracted common code into trait

--- src/TypeResolverDecorators/AbstractUserStateConfigurableAccessControlForDirectivesInPublicSchemaTypeResolverDecorator.php
+++ src/TypeResolverDecorators/AbstractUserStateConfigurableAccessControlForDirectivesInPublicSchemaTypeResolverDecorator.php
@@ -1,31 +1,18 @@
 <?php
 namespace PoP\UserStateAccessControl\TypeResolverDecorators;
 
-use PoP\ComponentModel\Facades\Schema\FieldQueryInterpreterFacade;
-use PoP\AccessControl\TypeResolverDecorators\AbstractConfigurableAccessControlForDirectivesInPublicSchemaTypeResolverDecorator;
 use PoP\AccessControl\Facades\AccessControlManagerFacade;
 use PoP\UserStateAccessControl\Services\AccessControlGroups;
+use PoP\AccessControl\TypeResolverDecorators\AbstractConfigurableAccessControlForDirectivesInPublicSchemaTypeResolverDecorator;
+use PoP\UserStateAccessControl\TypeResolverDecorators\UserStateConfigurableAccessControlInPublicSchemaTypeResolverDecoratorTrait;
 
 abstract class AbstractUserStateConfigurableAccessControlForDirectivesInPublicSchemaTypeResolverDecorator extends AbstractConfigurableAccessControlForDirectivesInPublicSchemaTypeResolverDecorator
 {
+    use UserStateConfigurableAccessControlInPublicSchemaTypeResolverDecoratorTrait;
+
     protected function getConfigurationEntries(): array
     {
         $accessControlManager = AccessControlManagerFacade::getInstance();
         return $accessControlManager->getEntriesForDirectives(AccessControlGroups::STATE);
     }
-
-    protected function getMandatoryDirectives($entryValue = null): array
-    {
-        $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
-        $validateUserStateDirectiveClass = $this->getValidateUserStateDirectiveResolverClass();
-        $validateUserStateDirectiveName = $validateUserStateDirectiveClass::getDirectiveName();
-        $validateUserStateDirective = $fieldQueryInterpreter->getDirective(
-            $validateUserStateDirectiveName
-        );
-        return [
-            $validateUserStateDirective,
-        ];
-    }
-
-    abstract protected function getValidateUserStateDirectiveResolverClass(): string;
 }
--- src/TypeResolverDecorators/AbstractUserStateConfigurableAccessControlForFieldsInPublicSchemaTypeResolverDecorator.php
+++ src/TypeResolverDecorators/AbstractUserStateConfigurableAccessControlForFieldsInPublicSchemaTypeResolverDecorator.php
@@ -3,29 +3,16 @@ namespace PoP\UserStateAccessControl\TypeResolverDecorators;
 
 use PoP\AccessControl\Facades\AccessControlManagerFacade;
 use PoP\UserStateAccessControl\Services\AccessControlGroups;
-use PoP\ComponentModel\Facades\Schema\FieldQueryInterpreterFacade;
 use PoP\AccessControl\TypeResolverDecorators\AbstractConfigurableAccessControlForFieldsInPublicSchemaTypeResolverDecorator;
+use PoP\UserStateAccessControl\TypeResolverDecorators\UserStateConfigurableAccessControlInPublicSchemaTypeResolverDecoratorTrait;
 
 abstract class AbstractUserStateConfigurableAccessControlForFieldsInPublicSchemaTypeResolverDecorator extends AbstractConfigurableAccessControlForFieldsInPublicSchemaTypeResolverDecorator
 {
+    use UserStateConfigurableAccessControlInPublicSchemaTypeResolverDecoratorTrait;
+
     protected static function getConfigurationEntries(): array
     {
         $accessControlManager = AccessControlManagerFacade::getInstance();
         return $accessControlManager->getEntriesForFields(AccessControlGroups::STATE);
     }
-
-    protected function getMandatoryDirectives($entryValue = null): array
-    {
-        $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
-        $validateUserStateDirectiveClass = $this->getValidateUserStateDirectiveResolverClass();
-        $validateUserStateDirectiveName = $validateUserStateDirectiveClass::getDirectiveName();
-        $validateUserStateDirective = $fieldQueryInterpreter->getDirective(
-            $validateUserStateDirectiveName
-        );
-        return [
-            $validateUserStateDirective,
-        ];
-    }
-
-    abstract protected function getValidateUserStateDirectiveResolverClass(): string;
 }
