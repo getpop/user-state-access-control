0d8648865461c9117410abe6d9994feea9d39ebe
Leo <losoviz@gmail.com>
2020-03-02 16:25:52 +0800

    Removed stale trait

--- src/TypeResolverDecorators/ValidateBasedOnUserStateForFieldsTypeResolverDecoratorTrait.php
+++ src/TypeResolverDecorators/ValidateBasedOnUserStateForFieldsTypeResolverDecoratorTrait.php
@@ -3,12 +3,9 @@ namespace PoP\UserStateAccessControl\TypeResolverDecorators;
 
 use PoP\AccessControl\Facades\AccessControlManagerFacade;
 use PoP\UserStateAccessControl\Services\AccessControlGroups;
-use PoP\AccessControl\TypeResolverDecorators\ValidateBasedOnConditionForFieldsTypeResolverDecoratorTrait;
 
 trait ValidateBasedOnUserStateForFieldsTypeResolverDecoratorTrait
 {
-    use ValidateBasedOnConditionForFieldsTypeResolverDecoratorTrait;
-
     protected static function getConfigurationEntries(): array
     {
         $accessControlManager = AccessControlManagerFacade::getInstance();
