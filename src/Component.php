65a665d122080520e78c8074a2593d903f98412a
Leonardo Losoviz <leo@getpop.org>
2020-05-20 18:49:04 +0800

    Added dependency to conditional components

--- src/Component.php
+++ src/Component.php
@@ -28,6 +28,18 @@ class Component extends AbstractComponent
     }
 
     /**
+     * All conditional component classes that this component depends upon, to initialize them
+     *
+     * @return array
+     */
+    public static function getDependedConditionalComponentClasses(): array
+    {
+        return [
+            \PoP\CacheControl\Component::class,
+        ];
+    }
+
+    /**
      * Initialize services
      */
     public static function init()
